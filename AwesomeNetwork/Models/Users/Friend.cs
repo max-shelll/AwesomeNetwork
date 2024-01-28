4f8a1b5eba672a4a669e19abf246a1dfe200c8fb
max-shelll <max-shelll@yandex.ru>
2024-01-28 19:59:39 +0300

    Update Friend.cs

--- AwesomeNetwork/Models/Users/Friend.cs
+++ AwesomeNetwork/Models/Users/Friend.cs
@@ -7,9 +7,9 @@ namespace AwesomeNetwork.Models.Users
 {
     public class Friend
     {
-       public int Id { get; set; }
-       public string UserId { get; set; }
-       public User User { get; set; }
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public User User { get; set; }
 
         public string CurrentFriendId { get; set; }
 
